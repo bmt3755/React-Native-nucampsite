7bb500e617eaf6a311fa37ca44c1bb67baaf05c1
Manjeera <manjeera.thogarcheti@gmail.com>
2021-08-30 12:21:24 -0400

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Constants from 'expo-constants';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
-import { CAMPSITES } from '../shared/campsites';
 
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
-    }, 
+    },
     {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
@@ -26,23 +27,52 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
-//Always wrap the top level navigator for your app with create app container
-const AppNavigator = createAppContainer(DirectoryNavigator);
+
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+
+const MainNavigator = createDrawerNavigator(
+    //first argument should contain the objects to the screens that will be in the drawer
+    //We want to route then through the stack navigator
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    //Optional: we are setting background color
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
+//Always wrap the top level navigator with the createAppContainer
+const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
-                    }}
-                >
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+            }}>
                 <AppNavigator />
             </View>
         );
     }
 }
-
 
 export default Main;
